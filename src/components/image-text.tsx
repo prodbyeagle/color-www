40d281cfb77be85ec2288082f399bdaf041708be
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-05-05 23:48:54 +0200

    feat: remove an duplicate

--- src/components/image-text.tsx
+++ src/components/image-text.tsx
@@ -10,7 +10,6 @@ export function ImageText({
 	const paintImages = [
 		'https://cdn.7tv.app/paint/01HVRVXQYG0004D7R0SPHMPE6Y/layer/01JAMR2GVKF88A52AP1QFK8KGB/4x.avif',
 		'https://cdn.7tv.app/paint/01J5EKNQS80000V182Y1H9Z6Y8/layer/01JAMR36JC0HBZQ4ZQVJJTPEJM/4x.avif',
-		'https://cdn.7tv.app/paint/01JEDE45FTB7XTQGRFXP0BA2PT/layer/01JF06XNCYM98WZAGNT0D51SQ3/4x.avif',
 		'https://cdn.7tv.app/paint/01JEY00EDNVW20AWX2NPG4HTNF/layer/01JH1Q77D54RJ8DKK9M5WCYR27/4x.avif',
 		'https://cdn.7tv.app/paint/01HBXZS5680009NC1Q0NCNE82C/layer/01JAMR2BPQYF8Y2KQZF5Q6BDXR/4x.avif',
 		'https://kappa.lol/gPbrzH',
