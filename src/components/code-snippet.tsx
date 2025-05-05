f9f5b8c97a39a7b16366c0ab163d8532b9aa9bf3
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-05-05 05:17:17 +0200

    feat: add CodeBlock component for syntax highlighting

---
+++ src/components/code-snippet.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { JSX, useEffect, useState } from 'react';
+import { BundledLanguage } from 'shiki';
+
+import { highlight } from '@/lib/shared';
+
+import { Pre } from './ui/pre';
+
+interface CodeBlockProps {
+	code: string;
+	lang: BundledLanguage;
+}
+
+export default function CodeBlock({
+	code = 'const colors = extractColor(imageData);',
+	lang = 'typescript',
+}: CodeBlockProps) {
+	const [highlightedCode, setHighlightedCode] = useState<JSX.Element | null>(
+		null
+	);
+
+	useEffect(() => {
+		return void highlight(code, lang).then(setHighlightedCode);
+	}, [code, lang]);
+
+	if (!highlightedCode) {
+		return <p>Loading...</p>;
+	}
+
+	return <Pre>{highlightedCode}</Pre>;
+}
