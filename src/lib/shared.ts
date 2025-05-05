2c2fa13d3022be40c69146777de14db530d7e081
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-05-05 05:17:02 +0200

    feat: implement highlight function for code syntax highlighting

---
+++ src/lib/shared.ts
@@ -0,0 +1,22 @@
+import { toJsxRuntime } from 'hast-util-to-jsx-runtime';
+import type { JSX } from 'react';
+import { Fragment } from 'react';
+import { jsx, jsxs } from 'react/jsx-runtime';
+import type { BundledLanguage } from 'shiki/bundle/web';
+import { codeToHast } from 'shiki/bundle/web';
+
+export async function highlight(code: string, lang: BundledLanguage) {
+	const out = await codeToHast(code, {
+		lang,
+		theme: 'vesper',
+		colorReplacements: {
+			'#101010': '#00000000',
+		},
+	});
+
+	return toJsxRuntime(out, {
+		Fragment,
+		jsx,
+		jsxs,
+	}) as JSX.Element;
+}
